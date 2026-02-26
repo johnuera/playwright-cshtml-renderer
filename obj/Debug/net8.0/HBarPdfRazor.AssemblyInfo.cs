a3fdea96963f32f9d550ad2de2058f4280607f3f
John Ben Uera <john.uera@fuljoyment.de>
2026-02-26 17:13:28 +0100

    add gitignore

--- obj/Debug/net8.0/HBarPdfRazor.AssemblyInfo.cs
+++ obj/Debug/net8.0/HBarPdfRazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HBarPdfRazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71e48b17c81af1a7533621290bbb316614e1e49")]
 [assembly: System.Reflection.AssemblyProductAttribute("HBarPdfRazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HBarPdfRazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
